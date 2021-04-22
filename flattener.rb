9a8dcb915dade3dda14eefe84cd4516e5062ae1e
Phil McCarthy <philmcc2631@gmail.com>
2021-04-22 15:27:04 -0600

    Add additional test cases with working recursive method

--- flattener.rb
+++ flattener.rb
@@ -1,3 +1,10 @@
-def flattener(array)
-  require 'pry'; binding.pry
+def flattener(array, results = [])
+  array.each do |element|
+    if element.class == Array
+      flattener(element, results)
+    else
+      results << element
+    end
+  end
+  results
 end
--- spec/flattener_spec.rb
+++ spec/flattener_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
-require 'flattener'
+require './flattener'
 
 RSpec.describe 'flattener' do
   describe 'flattens' do
     it 'a singularly nested array' do
       array = [ 0, 1, 2, [ 3, 4 ] ]
-      expect(flattener(array)).to eq(array.flatten)
+      expect(flattener(array)).to eq([0, 1, 2, 3, 4])
+    end
+
+    it 'a deeply nested array of strings' do
+      array_of_strings = ["hi", "this is", [[["string"], "that is very"], [[[["nested"]]]]]]
+
+      expect(flattener(array_of_strings)).to eq(["hi", "this is", "string", "that is very", "nested"])
+    end
+    
+    it 'a deeply nested array of ints' do
+      array_of_ints = [1, 2, 3, [[4], 5], [[[6]]]]
+      expect(flattener(array_of_ints)).to eq([1, 2, 3, 4, 5, 6])
     end
   end
 end
